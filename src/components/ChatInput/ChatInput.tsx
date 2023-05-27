f964c349547e61ae1aa2b2d1c0ce4597a236276e
Cyril Lazar <klazar@mail.ru>
2023-05-27 11:41:41 +0700

    feat:сделал отправку сообщения по ENter

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,4 +1,13 @@
-import { FC, useState, FormEvent, useEffect } from 'react';
+import {
+  FC,
+  useState,
+  FormEvent,
+  useEffect,
+  useCallback,
+  useRef,
+  BaseSyntheticEvent,
+  ReactElement,
+} from 'react';
 import './ChatInput.scss';
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
 
@@ -14,16 +23,20 @@ const ChatInput: FC<IChatInputProps> = ({
   onSubmitMessage,
 }) => {
   const [isEmpty, setIsEmpty] = useState(true);
+  const input = useRef<any>(null);
+  const [isEnter, setIsEnter] = useState(false);
 
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (!isEmpty) onSubmitMessage();
   }
 
-  const pasteAsPlainText = (e:React.ClipboardEvent<HTMLDivElement>) => {
-    e.preventDefault();   
-    setMessageInputValue(prev=>prev + e.clipboardData.getData("text/plain"))
-  }
+  const pasteAsPlainText = (e: React.ClipboardEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setMessageInputValue(
+      (prev) => prev + e.clipboardData.getData('text/plain')
+    );
+  };
 
   useEffect(() => {
     setIsEmpty(messageInputValue.length === 0);
@@ -31,12 +44,26 @@ const ChatInput: FC<IChatInputProps> = ({
 
   function onChange(e: ContentEditableEvent) {
     setMessageInputValue(e.target.value);
+    // input.current = e.target.value;
   }
 
+  const onKeyDownEnter = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && e.shiftKey) {
+      return;
+    }
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (!isEmpty) onSubmitMessage();
+    }
+  };
+
+  // use
   return (
-    <form className='chat-input' onSubmit={onSubmit}>
+    <form className='chat-input' onSubmit={onSubmit} onKeyDown={onKeyDownEnter}>
       <div className='chat-input__container'>
         <ContentEditable
+          // onKeyDown={onKeyDownEnter}
+          // ref={input}
           html={messageInputValue}
           disabled={false}
           onChange={onChange}
